Update to fix Injection plotting for fitting weights, and to reduce gamma precision from 0.1 to 0.025

analyses/tde/compare_flare_to_box.py
@@ -77,7 +77,7 @@ no_flare_negative = {
     "Fit Negative n_s?": True
 }
 
-flare_with_energy = {
+flare = {
     "LLH Energy PDF": llh_energy,
     "LLH Time PDF": llh_time,
     "Fit Gamma?": True,
@@ -87,16 +87,16 @@ flare_with_energy = {
 
 src_res = dict()
 
-lengths = np.logspace(-2, 0, 9) * max_window
+lengths = np.logspace(-2, 0, 3) * max_window
 
 for i, llh_kwargs in enumerate([no_flare,
                                 no_flare_negative,
-                                flare_with_energy
+                                flare
                                 ]):
 
     label = ["Time-Integrated", "Time-Integrated (negative n_s)",
              "Cluster Search"][i]
-    f_name = ["fixed_box", "fixed_box_negative", "flare_fit_gamma"][i]
+    f_name = ["fixed_box", "fixed_box_negative", "flare"][i]
 
     flare_name = name + f_name + "/"
 
@@ -134,7 +134,7 @@ for i, llh_kwargs in enumerate([no_flare,
             "llh kwargs": llh_kwargs,
             "scale": scale,
             "n_trials": 1,
-            "n_steps": 15
+            "n_steps": 5
         }
 
         analysis_path = analysis_dir + full_name
@@ -149,7 +149,7 @@ for i, llh_kwargs in enumerate([no_flare,
         with open(pkl_file, "wb") as f:
             Pickle.dump(mh_dict, f)
 
-        # rd.submit_to_cluster(pkl_file, n_jobs=500)
+        rd.submit_to_cluster(pkl_file, n_jobs=50)
 
         # mh = MinimisationHandler(mh_dict)
         # mh.iterate_run(mh_dict["scale"], mh_dict["n_steps"], n_trials=10)
@@ -159,7 +159,7 @@ for i, llh_kwargs in enumerate([no_flare,
 
     src_res[label] = res
 
-# rd.wait_for_cluster()
+rd.wait_for_cluster()
 
 sens = [[] for _ in src_res]
 fracs = [[] for _ in src_res]

analyses/tde/compare_spectral_indices.py
@@ -75,8 +75,6 @@ fixed_weights_negative = {
 gammas = [1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.5, 2.7]
 gammas = [1.8, 2.0, 2.3, 2.5, 2.7]
 gammas = [1.8, 2.0]
-# gammas = [1.99, 2.0, 2.02]
-# gammas = [2.5, 2.7, 2.9]
 
 # cats = ["gold", "jetted"]
 # # cats = ["jetted"]
@@ -108,11 +106,9 @@ for e_min in power_law_start_energy:
 
         closest_src = np.sort(catalogue, order="Distance (Mpc)")[0]
 
-        # lengths = [0.5 * max_window]
-
         for i, llh_kwargs in enumerate([fixed_weights_negative,
                                         fixed_weights,
-                                        # fit_weights,
+                                        fit_weights,
                                         # flare
                                         ]):
             label = ["Fixed Weights (Negative n_s)", "Fixed Weights",
@@ -157,7 +153,7 @@ for e_min in power_law_start_energy:
                     "llh kwargs": llh_kwargs,
                     "scale": scale,
                     "n_trials": 20,
-                    "n_steps": 5
+                    "n_steps": 15
                 }
 
                 analysis_path = analysis_dir + full_name
@@ -173,14 +169,12 @@ for e_min in power_law_start_energy:
                     Pickle.dump(mh_dict, f)
 
                 # if label == "Fit Weights":
-                # rd.submit_to_cluster(pkl_file, n_jobs=50)
+                rd.submit_to_cluster(pkl_file, n_jobs=500)
 
-                #     print catalogue
-                # #
-                mh = MinimisationHandler(mh_dict)
-                mh.iterate_run(mh_dict["scale"], mh_dict["n_steps"],
-                               n_trials=10)
-                mh.clear()
+                # mh = MinimisationHandler(mh_dict)
+                # mh.iterate_run(mh_dict["scale"], mh_dict["n_steps"],
+                #                n_trials=10)
+                # mh.clear()
 
                 res[gamma] = mh_dict
 
@@ -190,7 +184,7 @@ for e_min in power_law_start_energy:
 
     cutoff_dict[e_min] = cat_res
 
-# rd.wait_for_cluster()
+rd.wait_for_cluster()
 
 for (e_min, cat_res) in cutoff_dict.iteritems():
 
@@ -256,8 +250,6 @@ for (e_min, cat_res) in cutoff_dict.iteritems():
 
                 if len(f) > 0:
 
-                    # print f, fluence
-
                     ax1.plot(f, fluence[i], label=labels[i], linestyle=linestyle,
                              color=cols[i])
                     ax2.plot(f, energy[i], linestyle=linestyle,
@@ -277,7 +269,7 @@ for (e_min, cat_res) in cutoff_dict.iteritems():
                 y = [fluence, energy][k]
 
                 ax.set_ylim(0.95 * min([min(x) for x in y if len(x) > 0]),
-                             1.1 * max([max(x) for x in y if len(x) > 0]))
+                            1.1 * max([max(x) for x in y if len(x) > 0]))
 
             plt.title("Stacked " + ["Sensitivity", "Discovery Potential"][j] +
                       " for " + cat_names[b] + " TDEs")

core/minimisation.py
@@ -14,6 +14,14 @@ from shared import name_pickle_output_dir, fit_setup, inj_dir_name,\
     plot_output_dir, scale_shortener
 
 
+def time_smear(inj):
+    inj_time = inj["Injection Time PDF"]
+    max_length = inj_time["Max Offset"] - inj_time["Min Offset"]
+    offset = np.random.random() * max_length + inj_time["Min Offset"]
+    inj_time["Offset"] = offset
+    return inj_time
+
+
 class MinimisationHandler:
     """Generic Class to handle both dataset creation and llh minimisation from
     experimental data and Monte Carlo simulation. Initilialised with a set of
@@ -44,12 +52,7 @@ class MinimisationHandler:
             self.time_smear = False
 
         if self.time_smear:
-            inj_time = inj["Injection Time PDF"]
-            max_length = inj_time["Max Offset"] - inj_time["Min Offset"]
-            offset = np.random.random() * max_length + inj_time["Min Offset"]
-            inj_time["Offset"] = offset
-
-            inj["Injection Time PDF"] = inj_time
+            inj["Injection Time PDF"] = time_smear(inj)
 
         self.inj_kwargs = inj
         self.llh_kwargs = mh_dict["llh kwargs"]
@@ -176,6 +179,19 @@ class MinimisationHandler:
                 fe = [inj.time_pdf.sig_t1(source)
                       for inj in self.injectors.itervalues()]
                 true_fe = max(fe)
+
+                if self.time_smear:
+                    inj_time = self.inj_kwargs["Injection Time PDF"]
+                    offset = inj_time["Offset"]
+                    true_fs -= offset
+                    true_fe -= offset
+
+                    min_offset = inj_time["Min Offset"]
+                    max_offset = inj_time["Max Offset"]
+                    med_offset = 0.5*(max_offset + min_offset)
+
+                    true_fs += med_offset
+                    true_fe += med_offset
 
                 true_l = true_fe - true_fs
 

core/results.py
@@ -7,7 +7,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from shared import name_pickle_output_dir, plot_output_dir, k_to_flux, \
-    fit_setup, inj_dir_name
+    fit_setup, inj_dir_name, scale_shortener
 from ts_distributions import plot_background_ts_distribution, plot_fit_results
 from utils.neutrino_astronomy import calculate_astronomy
 
@@ -214,7 +214,7 @@ class ResultsHandler:
                 Pickle.dump(merged_data, mp)
 
             if len(merged_data.keys()) > 0:
-                self.results[float(sub_dir_name)] = merged_data
+                self.results[scale_shortener(float(sub_dir_name))] = merged_data
 
         if len(self.results.keys()) == 0:
             print "No data was found by ResultsHandler object!"
@@ -236,9 +236,9 @@ class ResultsHandler:
         """
 
         try:
-            bkg_dict = self.results[0.0]
+            bkg_dict = self.results[scale_shortener(0.0)]
         except KeyError:
-            print "No key equal to '0.0'"
+            print "No key equal to '0'"
             return
 
         bkg_ts = bkg_dict["TS"]
@@ -258,12 +258,12 @@ class ResultsHandler:
             print "above", "{0:.2f}".format(bkg_median),
             print "(", len(ts_array), ")"
 
-            if scale == 0.0:
+            if scale == scale_shortener(0.0):
                 self.frac_over = frac
 
             if len(ts_array) > 1:
                 y.append(frac)
-                x_acc.append(scale)
+                x_acc.append(float(scale))
 
                 self.make_plots(scale)
 
@@ -318,12 +318,12 @@ class ResultsHandler:
 
     def find_disc_potential(self):
 
-        ts_path = self.plot_dir + "ts_distributions/0.0.pdf"
+        ts_path = self.plot_dir + "ts_distributions/0.pdf"
 
         try:
-            bkg_dict = self.results[0.0]
+            bkg_dict = self.results[scale_shortener(0.0)]
         except KeyError:
-            print "No key equal to '0.0'"
+            print "No key equal to '0'"
             return
 
         bkg_ts = bkg_dict["TS"]
@@ -342,7 +342,7 @@ class ResultsHandler:
             print "Fraction of overfluctuations is", "{0:.2f}".format(frac)
             y.append(frac)
 
-        x = np.array(x)
+        x = np.array([float(s) for s in x])
 
         x_flux = k_to_flux(x)
 
@@ -424,12 +424,7 @@ class ResultsHandler:
                          ".pdf"
 
             if self.show_inj:
-                for key in self.inj.keys():
-                    if '{0:.4G}'.format(float(key)) == '{0:.4G}'.format(
-                            float(scale)):
-                        inj = self.inj[key]
-                        print inj
-                        raw_input("prompt")
+                inj = self.inj[str(scale)]
             else:
                 inj = None
 

utils/create_acceptance_functions.py
@@ -13,7 +13,7 @@ dec_range = np.arcsin(sin_dec_range)
 dec_edges = np.arcsin(sin_edges)
 
 # gamma_vals = np.linspace(gamma_range[0], gamma_range[1], 31)
-gamma_vals = np.linspace(0.5, 5.5, 51)
+gamma_vals = np.linspace(0.5, 5.5, 201)
 
 def make_acceptance_f(all_data):
     e_pdf = PowerLaw()

utils/make_SoB_splines.py
@@ -19,7 +19,6 @@ energy_bins = np.linspace(1., 10., 40 + 1)
 
 energy_pdf = PowerLaw()
 
-
 def _around(value):
     """Produces an array in which the precision of the value
     is rounded to the nearest integer. This is then multiplied
@@ -31,7 +30,7 @@ def _around(value):
     return np.around(float(value) / gamma_precision) * gamma_precision
 
 
-gamma_points = np.arange(0.7, 4.3, 0.1)
+gamma_points = np.arange(0.7, 4.3, gamma_precision)
 gamma_support_points = set([_around(i) for i in gamma_points])
 
 