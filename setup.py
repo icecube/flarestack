d722f46e2e23f454fd90dc0fd2843ce29670923c
Robert Stein <robert.stein@desy.de>
2018-10-01 16:15:49 +0200

    Update to include install requirements in setup.py

--- setup.py
+++ setup.py
@@ -17,12 +17,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flarestack",
-    version="0.0.1",
+    version="0.1",
     author="Robert Stein",
     author_email="robert.stein@desy.de",
-    description="Unbinned Likelihood Analysis code",
+    description="Unbinned likelihood analysis code",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="astroparticle physics science unbinned likelihood IceCube",
     url="https://github.com/robertdstein/flarestack",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -30,6 +32,12 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "numpy==1.14.0",
+        "numexpr==2.6.4",
+        "healpy==1.10.3",
+        "scipy==1.1.0"
+    ]
 )
 
 
