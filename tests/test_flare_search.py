e80cf3edbadf81c8eec5d4e3484e8fd9adb7c0e8
Robert Stein <robert.stein@desy.de>
2018-10-19 10:40:18 +0200

    Update to swap flare search test to using a generic ps source rather than a TDE source, and update corresponding best fit values

--- tests/test_flare_search.py
+++ tests/test_flare_search.py
@@ -1,11 +1,10 @@
 """Test the flare search method using one fixed-seed background trial. The
-process is deterministic, so the same flare should be found each time. The
-test is based on the flare search analysis of the TDE Swift J1644+57.
+process is deterministic, so the same flare should be found each time.
 """
 import numpy as np
 from flarestack.core.unblinding import Unblinder
-from flarestack.data.icecube.ps_tracks.ps_v002_p01 import IC79_dict
-from flarestack.shared import catalogue_dir
+from flarestack.data.icecube.ps_tracks.ps_v002_p01 import IC86_1_dict
+from flarestack.utils.prepare_catalogue import ps_catalogue_name
 import unittest
 
 analyses = dict()
@@ -31,30 +30,30 @@ unblind_llh = {
     "Flare Search?": True
 }
 
-name = "analyses/tde/unblind_individual_tdes/SwiftJ1644+57"
+name = "tests/test_flare_search/"
 
 
-cat_path = catalogue_dir + "TDEs/individual_TDEs/Swift J1644+57_catalogue.npy"
+cat_path = ps_catalogue_name(-0.1)
 catalogue = np.load(cat_path)
 
 unblind_dict = {
     "name": name,
-    "datasets": [IC79_dict],
+    "datasets": [IC86_1_dict],
     "catalogue": cat_path,
     "llh kwargs": unblind_llh,
 }
 
 # Inspecting the neutrino lightcurve for this fixed-seed scramble confirms
-# that the most significant flare is in a 2.5 day window. The best-fit
-# parameters are shown below. As fitting is deterministic, these values
-# should be returned every time this test is run.
+# that the most significant flare is in a 14 day window. The best-fit
+# parameters are shown below. As both the scrambling and fitting is
+# deterministic, these values should be returned every time this test is run.
 
 true_parameters = [
-    1.9803355734180155,
-    2.218092168368481,
-    55655.88411894165,
-    55658.743028581,
-    2.4960624171217205
+    4.470342243452785,
+    2.6270936625866463,
+    55876.89316064464,
+    55892.569503379375,
+    14.084548753227864
 ]
 
 
@@ -65,8 +64,12 @@ class TestFlareSearch(unittest.TestCase):
 
     def test_flare(self):
         ub = Unblinder(unblind_dict)
-        res = ub.res_dict["Swift J1644+57"]["Parameters"]
+        key = [x for x in ub.res_dict.iterkeys() if x != "TS"][0]
+        res = ub.res_dict[key]["Parameters"]
         self.assertEqual(res, true_parameters)
+
+        print "Best fit values", res
+        print "Reference best fit", res
 
 
 if __name__ == '__main__':
