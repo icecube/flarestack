e29f4e1f04da43ec0db3c52f83a5005d7e70f077
Robert Stein <robert.stein@desy.de>
2018-10-24 11:39:38 +0200

    Updated cosmology

--- flarestack/utils/neutrino_cosmology.py
+++ flarestack/utils/neutrino_cosmology.py
@@ -8,34 +8,38 @@ import os
 from flarestack.shared import plots_dir
 from flarestack.utils.neutrino_astronomy import fluence_integral
 
-# IceCube Diffuse Flux @ 100TeV
+# IceCube Diffuse Flux  best Fiy @ 100TeV
 
-diffuse_flux = 0.90 * 10**-18 * u.GeV**-1 * u.cm**-2 * u.s**-1 * u.sr**-1
+# diffuse_flux = 0.90 * 10**-18 * u.GeV**-1 * u.cm**-2 * u.s**-1 * u.sr**-1
+#
+# # IceCube Joint Best Fit
+#
+# diffuse_flux = 6.7 * 10**-18 * u.GeV**-1 * u.cm**-2 * u.s**-1 * u.sr**-1
+
+# IceCube Joint Best Fit
+
+diffuse_flux = 1.01 * 10**-18 * u.GeV**-1 * u.cm**-2 * u.s**-1 * u.sr**-1
 
 print "Diffuse Flux at 100 TeV:", diffuse_flux
 
 # Best Fit Spectral Index
 
-diffuse_gamma = 2.13
+# diffuse_gamma = 2.13
+#
+diffuse_gamma = 2.5
+
+diffuse_gamma = 2.19
 
 # Conversion to 1GeV
 
-diffuse_flux *= (4 * np.pi * u.sr) * (10**5) ** diffuse_gamma
+diffuse_flux *= (4 * np.pi * u.sr) * (10 ** 5) ** diffuse_gamma
 
-print "Diffuse Flux at 1 GeV:", diffuse_flux
+print "Diffuse Flux at 1 GeV:", diffuse_flux, diffuse_flux * \
+                                              fluence_integral(diffuse_gamma)
 
 diffuse_fluence = diffuse_flux.to("GeV-1 cm-2 yr-1") * (1./12.) * u.yr
 
 print "Time-Integrated Diffuse Flux in 1 month:", diffuse_fluence
-
-nu_e = 10**50 * u.erg
-
-savedir = plots_dir + "cosmology/"
-
-try:
-    os.makedirs(savedir)
-except OSError:
-    pass
 
 def sfr_madau(z):
     """
@@ -44,11 +48,36 @@ def sfr_madau(z):
     Madau & Dickinson 2014, Equation 15
     result is in solar masses/year/Mpc^3, assume this is proportional to number of sources
     """
-    rate = 2 * 10**-6 * (1+z)**2.7 / (1 + ((1+z)/2.9)**5.6) /(
+    rate = 0.015 * (1+z)**2.7 / (1 + ((1+z)/2.9)**5.6) /(
         u.Mpc**3 * u.year
     )
 
     return rate
+
+def ccsn_madau(z):
+    return 0.0068 * sfr_madau(z)
+
+def snIIn_madau(z):
+    return 0.064 * ccsn_madau(z)
+
+def sfr_clash_candels(z):
+    """
+    star formation history
+    http://arxiv.org/pdf/1403.0007v3.pdf
+    Madau & Dickinson 2014, Equation 15
+    result is in solar masses/year/Mpc^3, assume this is proportional to number of sources
+    """
+    rate = 0.015 * (1+z)**5.0 / (1 + ((1+z)/1.5)**6.1) /(
+        u.Mpc**3 * u.year
+    )
+
+    return rate
+
+def ccsn_clash_candels(z):
+    return 0.0091 * sfr_clash_candels(z) * 0.7**2
+
+def snIIn_clash_candels(z):
+    return 0.064 * ccsn_clash_candels(z)
 
 
 def integrate_over_z(f, zmin=0.0, zmax=8.0):
@@ -63,11 +92,42 @@ def integrate_over_z(f, zmin=0.0, zmax=8.0):
 
     return int_sum
 
-def calculate_transient(nu_e, rate, zmax=8., gamma=diffuse_gamma,
+def cumulative_z(f, zrange):
+
+    ints = []
+
+    nsteps = 1e3 + 1
+
+    if isinstance(zrange, np.ndarray):
+        step = zrange[1] - zrange[0]
+    else:
+        zrange, step = np.linspace(0.0, zrange, nsteps + 1, retstep=True)
+
+    int_sum = 0.0
+
+    for i, z in enumerate(zrange[1:-1]):
+        int_sum += 0.5 * step * (f(z) + f(zrange[i + 1]))
+        ints.append(astropy.units.quantity.Quantity(int_sum))
+
+    return ints
+
+
+def calculate_transient(nu_e, rate, name, zmax=8., gamma=diffuse_gamma,
                         nu_bright_fraction=1.0,
                         diffuse_fraction=None):
+
+    print "\n"
+    print name
+    print "\n"
     print "Neutrino Energy is", nu_e
     print "Rate is", rate(0.0)
+
+    savedir = plots_dir + "cosmology/" + name + "/"
+
+    try:
+        os.makedirs(savedir)
+    except OSError:
+        pass
 
     fluence_conversion = fluence_integral(gamma)
 
@@ -77,30 +137,14 @@ def calculate_transient(nu_e, rate, zmax=8., gamma=diffuse_gamma,
 
     def rate_per_z(z):
         return rate(z) * cosmo.differential_comoving_volume(z) * \
-               nu_bright_fraction * (4 * np.pi * u.sr)/(1 + z)
+               nu_bright_fraction * (4 * np.pi * u.sr)
 
     def nu_flux_per_z(z):
         return rate_per_z(z).to("s-1") * nu_e / (
-                4 * np.pi * Distance(z=z).to("cm")**2)
+                4 * np.pi * Distance(z=z).to("cm")**2 * (1 + z))
 
-    def cumulative_nu_flux(zrange):
-        ints = []
-
-        nsteps = 1e3 + 1
-
-        if isinstance(zrange, np.ndarray):
-            step = zrange[1] - zrange[0]
-        else:
-            zrange, step = np.linspace(0.0, zrange, nsteps + 1, retstep=True)
-
-        int_sum = 0.0
-
-        for i, z in enumerate(zrange[1:-1]):
-            int_sum += 0.5 * step * (nu_flux_per_z(z) +
-                                     nu_flux_per_z(zrange[i + 1]))
-            ints.append(astropy.units.quantity.Quantity(int_sum))
-
-        return ints
+    def cumulative_nu_flux(z):
+        return cumulative_z(nu_flux_per_z, z)
 
     nu_at_horizon = cumulative_nu_flux(8)[-1]
 
@@ -108,6 +152,7 @@ def calculate_transient(nu_e, rate, zmax=8., gamma=diffuse_gamma,
     print "Neutrino diffuse flux:", diffuse_flux
     ratio = nu_at_horizon.value / diffuse_flux.value
     print "Fraction of diffuse flux", ratio
+    print "Local rate at z=0.0", rate(0.0)
 
     if diffuse_fraction is not None:
         print "Scaling flux so that, at z=8, the contribution is equal to", \
@@ -117,10 +162,24 @@ def calculate_transient(nu_e, rate, zmax=8., gamma=diffuse_gamma,
             (nu_e * fluence_conversion).to("erg")
 
     plt.figure()
-    plt.plot(zrange, sfr_madau(zrange))
+    plt.plot(zrange, rate(zrange))
     plt.yscale("log")
     plt.xlabel("Redshift")
-    plt.savefig(savedir + 'sfr_madau.pdf')
+    plt.savefig(savedir + 'rate.pdf')
+    plt.close()
+
+    # print "Sanity Check:"
+    # print "Integrated Source Counts \n"
+    #
+    # for z in [0.01, 0.05, 0.1, 0.2, 0.3]:
+    #     print z, Distance(z=z).to("Mpc"), cumulative_z(rate_per_z, z)[-1]
+
+    plt.figure()
+    plt.plot(zrange[1:-1], [x.value for x in cumulative_z(rate_per_z, zrange)])
+    plt.yscale("log")
+    plt.ylabel("Cumulative Sources")
+    plt.xlabel("Redshift")
+    plt.savefig(savedir + 'integrated_source_count.pdf')
     plt.close()
 
     plt.figure()
@@ -144,7 +203,7 @@ def calculate_transient(nu_e, rate, zmax=8., gamma=diffuse_gamma,
 
     plt.figure()
     plt.plot(zrange[1:-1],
-             [nu_flux_per_z(z).value * step for z in zrange[1:-1]])
+             [nu_flux_per_z(z).value for z in zrange[1:-1]])
     plt.yscale("log")
     plt.xlabel("Redshift")
     plt.ylabel(
@@ -170,5 +229,19 @@ def calculate_transient(nu_e, rate, zmax=8., gamma=diffuse_gamma,
     plt.close()
 
 
-calculate_transient(nu_e, sfr_madau, zmax=8., diffuse_fraction=1.0)
+results = [
+    ["SNIIn", 5 * 10**49 * u.erg, 0.064],
+    ["SNIIP", 6 * 10**48 * u.erg, 0.52],
+    ["SN1bc", 4.5 * 10**48 * u.erg, 0.069 + 0.176]
+]
+
+for [name, nu_e, fraction] in results:
+
+    def f(z):
+        return fraction * ccsn_clash_candels(z)
+
+    calculate_transient(nu_e, f, name, zmax=0.3)
+
+calculate_transient(1 * u.erg, ccsn_clash_candels, "CCSN", zmax=0.3,
+                    diffuse_fraction=1.0)
 
