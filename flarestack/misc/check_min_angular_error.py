cb4b820a027001801b97917078d62d771f7db7e2
Robert Stein <robert.stein@desy.de>
2018-11-05 15:24:48 +0100

    Add script to check angulr error estimates

--- flarestack/misc/check_min_angular_error.py
+++ flarestack/misc/check_min_angular_error.py
@@ -37,6 +37,114 @@ for season in txs_sample_v1:
     print np.degrees(min_angular_err), "degrees"
     print "This is out of", len(mask), "total events"
 
+    print
+
+    bins = np.linspace(-1, 1., 10)
+
+    means = 0.5 * (bins[:-1] + bins[1:])
+    fracs = []
+
+    for i, lower in enumerate(bins[:-1]):
+        upper = bins[i + 1]
+        mask = np.sin(exp["dec"]) > lower
+        mask *= np.sin(exp["dec"]) < upper
+
+        band_data = exp["sigma"][mask]
+
+        frac = np.sum(band_data < min_angular_err) / float(len(band_data))
+
+        fracs.append(frac)
+
+    plt.figure()
+    plt.plot(means, fracs)
+    plt.xlabel(r"$\sin(\delta)$")
+    plt.ylabel(r"Fraction of events sub-0.2$^\circ$ error")
+    plt.savefig(sample_dir + season["Name"] + "_declination.pdf")
+    plt.close()
+
+    t = exp[season["MJD Time Key"]]
+
+    sidereal_day = 364./365.
+
+    res = t % sidereal_day
+
+    az_offset = 2.54199002505 + 2.907
+
+    az = az_offset + 2 * np.pi * res - exp["ra"]
+
+    while np.sum(az > 2 * np.pi) > 0:
+        az[az > 2 * np.pi] -= 2 * np.pi
+
+    while np.sum(az < 0) > 0:
+        az[az < 0] += 2 * np.pi
+
+    bins = np.linspace(0, 2*np.pi, 50)
+
+    means = 0.5 * (bins[:-1] + bins[1:])
+    fracs_north = []
+    fracs_south = []
+
+    for i, lower in enumerate(bins[:-1]):
+        upper = bins[i + 1]
+        mask = az > lower
+        mask *= az < upper
+
+        band_data = exp["sigma"][mask]
+
+        north = band_data[exp["dec"][mask] > 0.]
+        south = band_data[exp["dec"][mask] < 0.]
+
+        frac_n = np.sum(north < min_angular_err) / float(len(north))
+
+        fracs_north.append(frac_n)
+
+        frac_s = np.sum(south < min_angular_err) / float(len(south))
+        fracs_south.append(frac_s)
+
+    plt.figure()
+    plt.plot(means, fracs_north, label="Northern Sky")
+    plt.plot(means, fracs_south, label="Southern Sky")
+    plt.xlabel("azimuth")
+    plt.ylabel(r"Fraction of events sub-0.2$^\circ$ error")
+    plt.legend()
+    plt.savefig(sample_dir + season["Name"] + "_azimuth.pdf")
+    plt.close()
+
+    bins = np.array(list(np.linspace(2.5, 5.0, 6)) + [7.0])
+
+    means = 0.5 * (bins[:-1] + bins[1:])
+    fracs_north = []
+    fracs_south = []
+
+    for i, lower in enumerate(bins[:-1]):
+        upper = bins[i + 1]
+        mask = exp["logE"] > lower
+        mask *= exp["logE"] < upper
+
+        band_data = exp["sigma"][mask]
+
+        north = band_data[exp["dec"][mask] > 0.]
+        south = band_data[exp["dec"][mask] < 0.]
+
+        frac_n = np.sum(north < min_angular_err) / float(len(north))
+
+        fracs_north.append(frac_n)
+
+        frac_s = np.sum(south < min_angular_err) / float(len(south))
+        fracs_south.append(frac_s)
+
+        print "North", np.sum(north < min_angular_err), float(len(north)), \
+            frac_n
+
+    plt.figure()
+    plt.plot(means, fracs_north, label="Northern Sky")
+    plt.plot(means, fracs_south, label="Southern Sky")
+    plt.xlabel("log(E)")
+    plt.ylabel(r"Fraction of events sub-0.2$^\circ$ error")
+    plt.legend()
+    plt.savefig(sample_dir + season["Name"] + "_energy.pdf")
+    plt.close()
+
     exp = data_loader(season["exp_path"])
 
     mask = exp["sigma"] < min_angular_err
