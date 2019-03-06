b02be820e4ef69a5e4bb3cb5857d92764acc5b94
Robert Stein <robert.stein@desy.de>
2019-03-06 14:19:10 +0100

    Change automated plotting of limits, so that it does not occur if there are no limits.

--- flarestack/analyses/tde/shared_TDE.py
+++ flarestack/analyses/tde/shared_TDE.py
@@ -64,30 +64,30 @@ def make_limit_plot(name):
     path = limits_dir + "analyses/tde/compare_spectral_indices/Emin=100/" +\
         name + "/fit_weights/real_unblind/limit.pkl"
 
-    if not os.path.isfile(path):
-        raise Exception("{0} file not found".format(path))
+    if os.path.isfile(path):
 
-    with open(path, "r") as f:
-        data = Pickle.load(f)
+        with open(path, "r") as f:
+            data = Pickle.load(f)
 
-    savepath = os.path.dirname(path) + "/" + name + "_limit_plot.pdf"
+        savepath = os.path.dirname(path) + "/" + name + "_limit_plot.pdf"
 
-    f = interp1d(data["x"], np.log(data["energy"]))
+        f = interp1d(data["x"], np.log(data["energy"]))
 
-    x_range = np.linspace(data["x"][0], data["x"][-1], 10)
+        x_range = np.linspace(data["x"][0], data["x"][-1], 10)
 
-    plt.figure()
-    plt.errorbar(x_range, np.exp(f(x_range)),
-                 yerr=.25 * np.exp(f(x_range)),
-                 uplims=True)
-    plt.plot(x_range, np.exp(f(x_range)))
-    plt.ylabel(r"Isotropic-Equivalent $E_{\nu}$ (erg)")
-    plt.xlabel(r"Spectral Index ($\gamma$)")
-    plt.yscale("log")
-    plt.title("Per-source limits for {0} TDEs".format(name))
-    plt.tight_layout()
-    plt.savefig(savepath)
-    plt.close()
+        plt.figure()
+        plt.errorbar(x_range, np.exp(f(x_range)),
+                     yerr=.25 * np.exp(f(x_range)),
+                     uplims=True)
+        plt.plot(x_range, np.exp(f(x_range)))
+        plt.ylabel(r"Isotropic-Equivalent $E_{\nu}$ (erg)")
+        plt.xlabel(r"Spectral Index ($\gamma$)")
+        plt.yscale("log")
+        plt.title("Per-source limits for {0} TDEs".format(name))
+        plt.tight_layout()
+        plt.savefig(savepath)
+        plt.close()
+
 
 for cat in tde_catalogues:
     make_limit_plot(cat)
