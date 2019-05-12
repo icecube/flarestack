2f5bafbb4fc4392bb7da91c3e7d27b717bf5b93a
Robert Stein <robert.stein@desy.de>
2019-05-12 22:25:38 +0200

    Rename submit to generic 'analyse' function that can either submit to cluster or run locally depending on boolean 'cluster' kwarg.

--- flarestack/cluster/__init__.py
+++ flarestack/cluster/__init__.py
@@ -1,12 +1,14 @@
 import time
 import os
 from flarestack.shared import host_server
-from flarestack.cluster.run_desy_cluster import submit_to_cluster
+from flarestack.cluster.run_desy_cluster import submit_to_cluster,\
+    wait_for_cluster
 from flarestack.cluster.make_local_bash_script import local_submit_file,\
     make_local_submit_file
 
 if host_server == "DESY":
     submit_cluster = submit_to_cluster
+    wait_cluster = wait_for_cluster
 
 else:
     def submit_cluster(path, **kwargs):
@@ -26,9 +28,12 @@ def submit_local(path, n_cpu):
     os.system(submit_cmd)
 
 
-def submit(path, cluster=False, n_cpu=2, **kwargs):
+def analyse(paths, cluster=False, n_cpu=2, **kwargs):
+
+    print(paths)
+
     if cluster:
-        submit_cluster(path, n_cpu=n_cpu, **kwargs)
+        submit_cluster(paths, n_cpu=n_cpu, **kwargs)
     else:
-        submit_local(path, n_cpu=n_cpu)
+        submit_local(paths, n_cpu=n_cpu)
 
