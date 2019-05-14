e6e8db762bcaf53a1141dcfb8ceee4e15b8b7525
Robert Stein <robert.stein@desy.de>
2019-05-14 17:22:27 +0200

    Modify 'analyse' function. It now directly takes a MinimisationHandler dictionary as an argument, and relevant pickle files are made accordingly.

--- flarestack/cluster/__init__.py
+++ flarestack/cluster/__init__.py
@@ -1,6 +1,6 @@
 import time
 import os
-from flarestack.shared import host_server
+from flarestack.shared import host_server, make_analysis_pickle
 from flarestack.cluster.run_desy_cluster import submit_to_cluster,\
     wait_for_cluster
 from flarestack.cluster.make_local_bash_script import local_submit_file,\
@@ -28,12 +28,23 @@ def submit_local(path, n_cpu):
     os.system(submit_cmd)
 
 
-def analyse(paths, cluster=False, n_cpu=2, **kwargs):
+def analyse(mh_dict, cluster=False, n_cpu=2, **kwargs):
+    """Generic function to run an analysis on a given MinimisationHandler
+    dictionary. Can either run on cluster, or locally, based on the bolean
+    cluster arg. The number of cpus can be specified, as well as specific
+    kwargs such as number of jobs to run.
 
-    print(paths)
+    :param mh_dict: MinimisationHandler dictionary
+    :param cluster: Boolean flag for whether to run on cluster or locally.
+    Default is False (i.e locally)
+    :param n_cpu: Number of CPUs to run with. Default is 2.
+    :param kwargs: Optional kwargs
+    """
+
+    path = make_analysis_pickle(mh_dict)
 
     if cluster:
-        submit_cluster(paths, n_cpu=n_cpu, **kwargs)
+        submit_cluster(path, n_cpu=n_cpu, **kwargs)
     else:
-        submit_local(paths, n_cpu=n_cpu)
+        submit_local(path, n_cpu=n_cpu)
 
