2ff80d18bcdb8021629caf36e00d90e0bf57e070
Robert Stein <robert.stein@desy.de>
2019-06-27 16:13:08 +0200

    Rename 'nicecube' to 'simcube'

--- flarestack/data/simulate/simcube.py
+++ flarestack/data/simulate/simcube.py
@@ -3,11 +3,12 @@ from flarestack.data.simulate import generate_sim_season_class
 
 
 print(ic_sim_seasons)
-NiceCubeSeason = ic_sim_seasons['IC86-2012']
+SimcubeSeason = ic_sim_seasons['IC86-2012']
 
 e_pdf_dict = {
     "name": "PowerLaw",
     "gamma": 3.7
 }
-nicecube_10year = NiceCubeSeason(0, 100, 1., e_pdf_dict)
+
+nicecube_10year = SimcubeSeason(0, 100, 1., e_pdf_dict)
 
