565eea5892f2fc9ba13809528d593ff097efe8c1
Robert Stein <robert.stein@desy.de>
2019-09-07 00:31:17 +0200

    Update naming convention to be pythonic.

--- flarestack/data/icecube/__init__.py
+++ flarestack/data/icecube/__init__.py
@@ -6,7 +6,7 @@ from flarestack.data.icecube.gfu.gfu_v002_p01 import gfu_v002_p01, txs_sample_v1
 from flarestack.data.icecube.gfu.gfu_v002_p02 import gfu_v002_p02, txs_sample_v2
 from flarestack.data.icecube.gfu.gfu_v002_p04 import gfu_v002_p04
 
-ps_7year = ps_v002_p01
-ps_10year = ps_v003_p02
+ps_7_year = ps_v002_p01
+ps_10_year = ps_v003_p02
 diffuse_8_year = nt_v002_p05
 
