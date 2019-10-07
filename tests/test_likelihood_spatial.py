7e49f26975b0c99a6d0cd870912ed9be3ff7458f
Robert Stein <robert.stein@desy.de>
2019-10-07 10:45:09 +0200

    Update test_likelihood_spatial

--- tests/test_likelihood_spatial.py
+++ tests/test_likelihood_spatial.py
@@ -6,9 +6,12 @@ import unittest
 
 
 llh_dict = {
-    "name": "spatial",
-    "llh_time_pdf": {
-        "time_pdf_name": "Steady"
+    "llh_name": "spatial",
+    "llh_sig_time_pdf": {
+        "time_pdf_name": "steady"
+    },
+    "llh_bkg_time_pdf": {
+        "time_pdf_name": "steady"
     }
 }
 
@@ -16,12 +19,12 @@ source = ps_catalogue_name(0.0)
 
 unblind_dict = {
     "mh_name": "fixed_weights",
-    "datasets": ps_v002_p01.get_seasons("IC86_1"),
+    "dataset": ps_v002_p01.get_seasons("IC86_1"),
     "catalogue": ps_catalogue_name(0.5),
     "llh_dict": llh_dict
 }
 
-true_parameters = [2.129289647832715]
+true_parameters = [2.1292922213980687]
 
 
 class TestSpatialLikelihood(unittest.TestCase):
