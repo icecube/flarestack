4849df25e67a1b919a3ed0387182a6526f3cad8a
Robert Stein <robert.stein@desy.de>
2019-11-13 15:31:41 +0100

    Fix typo in minimisation.

--- flarestack/core/minimisation.py
+++ flarestack/core/minimisation.py
@@ -521,8 +521,8 @@ class FixedWeightMinimisationHandler(MinimisationHandler):
             flags.append(res_dict["Flag"])
 
         n_inj = 0
-        for season in self.mh.seasons.keys():
-            inj = self.mh.get_injector(season)
+        for season in self.seasons.keys():
+            inj = self.get_injector(season)
             for val in inj.ref_fluxes[scale_shortener(scale)].values():
                 n_inj += val
 
