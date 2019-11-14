1ea0e1a24fa5150dbc1b82de2f855e4b9d14ed5e
Robert Stein <robert.stein@desy.de>
2019-11-14 13:16:00 +0100

    Update test for fit weights.

--- tests/test_mh_fit_weights.py
+++ tests/test_mh_fit_weights.py
@@ -6,6 +6,7 @@ import unittest
 from flarestack.data.public import icecube_ps_3_year
 from flarestack.core.unblinding import create_unblinder
 from flarestack.analyses.tde.shared_TDE import tde_catalogue_name
+from flarestack import MinimisationHandler, analyse
 
 # Initialise Injectors/LLHs
 
@@ -43,6 +44,7 @@ class TestTimeIntegrated(unittest.TestCase):
         # Test three declinations
 
         unblind_dict = {
+            "name": "tests/test_mh_fit_weights",
             "mh_name": mh_name,
             "dataset": icecube_ps_3_year.get_seasons("IC86-2011"),
             "catalogue": catalogue,
@@ -56,6 +58,29 @@ class TestTimeIntegrated(unittest.TestCase):
             self.assertAlmostEqual(x, true_parameters[i], delta=5)
         logging.info("Best fit values {0}".format(list(res)))
         logging.info("Reference best fit {0}".format(true_parameters))
+
+        inj_dict = {
+            "injection_sig_time_pdf": {
+                "time_pdf_name": "steady"
+            },
+            "injection_bkg_time_pdf": {
+                "time_pdf_name": "steady",
+            },
+            "injection_energy_pdf": {
+                "energy_pdf_name": "power_law",
+                "gamma": 2.0
+            }
+        }
+
+        mh_dict = dict(unblind_dict)
+        mh_dict["inj_dict"] = inj_dict
+        mh_dict["n_trials"] = 1.
+        mh_dict["n_steps"] = 3.
+        mh_dict["scale"] = 5.
+
+        mh = MinimisationHandler.create(mh_dict)
+        res = mh.simulate_and_run(5.)
+        analyse(mh_dict, cluster=False)
 
 if __name__ == '__main__':
     unittest.main()
