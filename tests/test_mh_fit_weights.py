0a5f4ad450ecd71bfc5140a209a10c08781b8936
Robert Stein <robert.stein@desy.de>
2020-01-22 15:43:06 +0100

    Ecven more unittest mods.

--- tests/test_mh_fit_weights.py
+++ tests/test_mh_fit_weights.py
@@ -11,7 +11,7 @@ from flarestack import MinimisationHandler, analyse
 # Initialise Injectors/LLHs
 
 llh_dict = {
-    "llh_name": "standard_nonoverlapping",
+    "llh_name": "standard",
     "llh_sig_time_pdf": {
         "time_pdf_name": "steady"
     },
