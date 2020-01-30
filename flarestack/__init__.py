98a4f2391243630f652b83b589f3f641a6a0c132
Robert Stein <robert.stein@desy.de>
2020-01-30 11:22:08 +0100

    Import wait_cluster.

--- flarestack/__init__.py
+++ flarestack/__init__.py
@@ -1,5 +1,5 @@
 from flarestack.core.minimisation import MinimisationHandler
 from flarestack.core.results import ResultsHandler
-from flarestack.cluster import analyse
+from flarestack.cluster import analyse, wait_cluster
 from flarestack.core.unblinding import create_unblinder
 from flarestack.shared import fs_scratch_dir, fs_dir
