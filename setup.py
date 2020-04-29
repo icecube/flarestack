2c30561ca8c5a24a8a528a81ef3ac7ca50fa7c73
JannisNe <necker@physik.hu-berlin.de>
2020-04-29 20:24:42 +0200

    Release Flarestack v2.1.0 (Titan)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flarestack",
-    version="2.0.2",
+    version="2.1.0",
     author="Robert Stein",
     author_email="robert.stein@desy.de",
     description="Package for unbinned likelihood analysis of physics data",
