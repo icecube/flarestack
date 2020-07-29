Fix bug with units in butterfly plots

flarestack/cosmo/icecube_diffuse_flux/joint_15.py
@@ -9,7 +9,7 @@ C
 import numpy as np
 from astropy import units as u
 
-contour_68 = [
+contour_68 = np.array([
     (2.362728669872589, 6.014203559565713),
     (2.362693966585692, 5.8056368053145615),
     (2.363985424619503, 5.567299588518169),
@@ -46,9 +46,9 @@ contour_68 = [
     (2.3788186009617767, 6.714689405582271),
     (2.370787268851321, 6.4463834217440885),
     (2.3680928065043876, 6.2526647166724505),
-]
+])
 
-contour_95 = [
+contour_95 = np.array([
     (2.534660033167496, 9.597014925373134),
     (2.5014925373134327, 9.53731343283582),
     (2.4723051409618573, 9.373134328358208),
@@ -87,11 +87,14 @@ contour_95 = [
     (2.619568822553897, 9.17910447761194),
     (2.594361525704809, 9.402985074626866),
     (2.5678275290215584, 9.53731343283582),
-]
+])
 
 # Convert from all-flavour to muon-neutrino flux
 
 units = 10 ** -18 / 3. #/ u.GeV /u.cm**2 / u.s / u.sr
+
+for contour in [contour_68, contour_95]:
+    contour.T[1] *= units
 
 # IceCube Joint Best Fit
 # (https://arxiv.org/abs/1507.03991)
@@ -105,6 +108,6 @@ best_fit_gamma = 2.5
 e_range = np.logspace(np.log10(25) + 3, np.log10(2.8) + 6, 100)
 
 joint_15 = {
-    "joint_15": (best_fit_flux, best_fit_gamma, np.array(contour_68)*units, np.array(contour_95)*units, e_range)
+    "joint_15": (best_fit_flux, best_fit_gamma, contour_68, contour_95, e_range)
 }
 

flarestack/cosmo/icecube_diffuse_flux/nt_16.py
@@ -9,7 +9,7 @@ for the 6years Northern Track sample, tracing the 68% and 95% contours in Figure
 import numpy as np
 from astropy import units as u
 
-contour_68 = [
+contour_68 = np.array([
     (1.9321029740384577, 0.5435661287274187),
     (1.9397702849315752, 0.6304832917736136),
     (1.9538255805117974, 0.4926978308209975),
@@ -44,9 +44,9 @@ contour_68 = [
     (2.3213704458865747, 1.0767793148438303),
     (2.323989988506117, 1.172941879393492),
     (2.325327012423786, 1.1221276543857184),
-]
+])
 
-contour_95 = [
+contour_95 = np.array([
     (1.8106484596220667, 0.31415018365164915),
     (1.8244061024706184, 0.4847774196161283),
     (1.8383586585932625, 0.29723261600094375),
@@ -105,10 +105,13 @@ contour_95 = [
     (1.7988125071458405, 0.3703905370572027),
     (1.8130521047187713, 0.4308697642030963),
     (1.839755256421923, 0.5417448750782072),
-]
+])
 
 
 units = 10 ** -18  #/ u.GeV /u.cm**2 / u.s / u.sr
+
+for contour in [contour_68, contour_95]:
+    contour.T[1] *= units
 
 best_fit_flux = 0.90 * units * (
         u.GeV ** -1 * u.cm ** -2 * u.s ** -1 * u.sr ** -1
@@ -119,6 +122,6 @@ best_fit_gamma = 2.13
 e_range = np.logspace(np.log10(194) + 3, np.log10(7.8) + 6, 100)
 
 nt_16 = {
-    "northern_tracks_16": (best_fit_flux, best_fit_gamma, np.array(contour_68)*units, np.array(contour_68)*units, e_range)
+    "northern_tracks_16": (best_fit_flux, best_fit_gamma, contour_68, contour_68, e_range)
 }
 

flarestack/cosmo/icecube_diffuse_flux/nt_17.py
@@ -9,7 +9,7 @@ for the 8years Northern Track sample, tracing the 68% and 95% contours in Figure
 import numpy as np
 from astropy import units as u
 
-contour_68 = [
+contour_68 = np.array([
     (2.03012543242526, 0.7423683777687273),
     (2.034251572561308, 0.8074403176791254),
     (2.041619679947108, 0.6850712670424288),
@@ -44,9 +44,9 @@ contour_68 = [
     (2.3338879395838474, 1.157279461292647),
     (2.3348703539019544, 1.2496196426893074),
     (2.335607164640534, 1.2011255541370345),
-]
+])
 
-contour_95 = [
+contour_95 = np.array([
     (1.9285527229722836, 0.5643923966631448),
     (1.9361083821824496, 0.5099685923744479),
     (1.9338611094297806, 0.6268656844277705),
@@ -112,11 +112,13 @@ contour_95 = [
     (2.425813850778116, 1.3410302249443906),
     (2.4271190583721722, 1.2874853143895486),
     (2.4267260926449294, 1.2480703107866788),
-]
+])
 
 
 units = 10 ** -18  #/ u.GeV /u.cm**2 / u.s / u.sr
 
+for contour in [contour_68, contour_95]:
+    contour.T[1] *= units
 
 # Fit is valid from 119 TeV to 4.8 PeV.
 e_range = np.logspace(np.log10(119) + 3, np.log10(4.8) + 6, 100)
@@ -127,6 +129,6 @@ best_fit_flux = 1.01 * units * (
 best_fit_gamma = 2.19
 
 nt_17 = {
-    "northern_tracks_17": (best_fit_flux, best_fit_gamma, np.array(contour_68)*units, np.array(contour_68)*units, e_range)
+    "northern_tracks_17": (best_fit_flux, best_fit_gamma, contour_68, contour_68, e_range)
 }
 

flarestack/cosmo/icecube_diffuse_flux/nt_19.py
@@ -9,7 +9,7 @@ for the 9.5years Northern Track sample, tracing the 68% and 95% contours in Figu
 import numpy as np
 from astropy import units as u
 
-contour_68 = [
+contour_68 = np.array([
     (2.1474761536053775, 1.1819099315348742),
     (2.15745389846805, 1.3039308554297238),
     (2.16382267178465, 1.0801421376692102),
@@ -40,9 +40,9 @@ contour_68 = [
     (2.4018201924185765, 1.535833378794905),
     (2.404350010708226, 1.6800755571315573),
     (2.4059068219633946, 1.5907162925179348),
-]
+])
 
-contour_95 = [
+contour_95 = np.array([
 
     (2.0552051480586435, 0.9918704219323407),
     (2.0610731289435105, 0.8727567814199388),
@@ -95,10 +95,12 @@ contour_95 = [
     (2.4829689790942493, 1.8005509942445652),
     (2.4860826016045867, 1.747892856168681),
     (2.4860826016045867, 1.6484683996617662),
-]
-
+])
 
 units = 10 ** -18  #/ u.GeV /u.cm**2 / u.s / u.sr
+
+for contour in [contour_68, contour_95]:
+    contour.T[1] *= units
 
 # Fit is valid from 40 TeV to 3.5 PeV.
 e_range = np.logspace(np.log10(40) + 3, np.log10(3.5) + 6, 100)
@@ -109,6 +111,6 @@ best_fit_flux = 1.44 * units * (
 best_fit_gamma = 2.28
 
 nt_19 = {
-    "northern_tracks_19": (best_fit_flux, best_fit_gamma, np.array(contour_68)*units, np.array(contour_68)*units, e_range)
+    "northern_tracks_19": (best_fit_flux, best_fit_gamma, contour_68, contour_68, e_range)
 }
 