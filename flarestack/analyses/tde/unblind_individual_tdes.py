65515789771236e4e074fa03800c646e52d5ada5
Robert Stein <robert.stein@desy.de>
2020-08-03 15:57:45 +0100

    Update to v2 firmat.

--- flarestack/analyses/tde/unblind_individual_tdes.py
+++ flarestack/analyses/tde/unblind_individual_tdes.py
@@ -5,11 +5,14 @@ plots.
 """
 import numpy as np
 from flarestack.core.unblinding import create_unblinder
-from flarestack.data.icecube.gfu.gfu_v002_p01 import txs_sample_v1
-from flarestack.data.icecube.gfu.gfu_v002_p04 import gfu_v002_p04
-from flarestack.analyses.tde.shared_TDE import individual_tde_cat, \
-    individual_tdes
+from flarestack.data.icecube import txs_sample_v1, gfu_v002_p04
 from flarestack.utils.custom_dataset import custom_dataset
+from flarestack.analyses.tde.shared_TDE import individual_tdes, \
+    individual_tde_cat
+import logging
+from flarestack.utils.catalogue_loader import load_catalogue
+
+logging.getLogger().setLevel("INFO")
 
 analyses = dict()
 
@@ -18,20 +21,23 @@ analyses = dict()
 # Shared
 
 llh_energy = {
-    "Name": "Power Law",
-    "Gamma": 2.0,
+    "energy_pdf_name": "power_law",
+    "gamma": 2.0,
 }
 
 llh_time = {
-    "Name": "FixedEndBox"
+    "time_pdf_name": "custom_source_box"
 }
 
-unblind_llh = {
-    "LLH Energy PDF": llh_energy,
-    "LLH Time PDF": llh_time,
-    "Fit Gamma?": True,
-    "Fit Negative n_s?": False,
-    "Flare Search?": True
+llh_bkg_time = {
+    "time_pdf_name": "steady"
+}
+
+llh_dict = {
+    "llh_name": "standard",
+    "llh_energy_pdf": llh_energy,
+    "llh_sig_time_pdf": llh_time,
+    "llh_bkg_time_pdf": llh_bkg_time
 }
 
 name_root = "analyses/tde/unblind_individual_tdes/"
@@ -39,27 +45,58 @@ bkg_ts_root = "analyses/tde/compare_cluster_search_to_time_integration/"
 
 cat_res = dict()
 
-for j, cat in enumerate(individual_tdes):
+res = []
 
-    name = name_root + cat.replace(" ", "") + "/"
+for j, cat in enumerate(individual_tdes)[:1]:
 
-    bkg_ts = bkg_ts_root + cat.replace(" ", "") + "/flare/"
+    name = f"{name_root}/{cat.replace(' ', '')}/"
+
+    bkg_ts = f"{bkg_ts_root}/{cat.replace(' ', '')}/flare/"
 
     cat_path = individual_tde_cat(cat)
     catalogue = np.load(cat_path)
 
     if cat != "AT2018cow":
         dataset = custom_dataset(txs_sample_v1, catalogue,
-                                 unblind_llh["LLH Time PDF"])
+                                  llh_dict["llh_sig_time_pdf"])
     else:
         dataset = gfu_v002_p04
 
+    logging.info(f"{name}")
+
     unblind_dict = {
         "name": name,
-        "datasets": dataset,
+        "mh_name": "flare",
+        "dataset": dataset,
         "catalogue": cat_path,
-        "llh kwargs": unblind_llh,
-        "background TS": bkg_ts
+        "llh_dict": llh_dict,
+        "background_ts": bkg_ts
     }
 
-    ub = create_unblinder(unblind_dict, mock_unblind=False, full_plots=True)
+    ub = create_unblinder(unblind_dict, mock_unblind=False, disable_warning=True)
+
+    r = ub.res_dict
+    print(r)
+    ns = np.zeros(len(catalogue))
+
+    for x, val in r["Parameters"].items():
+        if "n_s" in x:
+            mask = np.array([str(y) in str(x) for y in catalogue["source_name"]])
+            ns[mask] = float(val)
+    print(f"nstot: {np.sum(ns)}")
+
+    res.append((cat, ub.ts))
+
+    print("Catalogue for latex export")
+
+    print("Source & R.A & Dec & Distance & T$_{0}$ & T$_{1}$ & n$_{s}$ \\\\")
+    print("& (deg.) & (deg.) & (Mpc) & (MJD) & (MJD) & \\\\")
+    print("\hline")
+
+    for i, x in enumerate(catalogue):
+        print(f'{x["source_name"].decode()} & {np.degrees(x["ra_rad"]):.2f} & {np.degrees(x["dec_rad"]):.2f} & '
+              f'{x["distance_mpc"]:.0f} & {x["start_time_mjd"]:.0f} & {x["end_time_mjd"]:.0f}  & {ns[i]:.2f} \\\\')
+    print("\hline")
+
+for x in res:
+    print(x)
