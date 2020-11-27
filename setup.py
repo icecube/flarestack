2ce8b0ba6eb6b0b60fd68d22ff9661fbd06e3d83
JannisNe <necker@physik.hu-berlin.de>
2020-11-27 12:18:27 +0100

    moving to higher numpy version as needed by astropy

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        "numpy>=1.16.0",
+        "numpy>=1.17.0",
         "numexpr",
         "healpy",
         "scipy",
