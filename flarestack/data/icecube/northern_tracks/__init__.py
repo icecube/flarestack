86f23672e677539c3a7d57a7af673b14b9991a9d
JannisNe <necker@physik.hu-berlin.de>
2022-02-14 17:58:10 +0100

    add ntv05p00

--- flarestack/data/icecube/northern_tracks/__init__.py
+++ flarestack/data/icecube/northern_tracks/__init__.py
@@ -5,41 +5,29 @@ from numpy.lib.recfunctions import rename_fields
 
 diffuse_binning = {
     "IC59": (
-        np.unique(
-            np.concatenate(
-                [
-                    np.linspace(0.0, 0.2, 14 + 1),
-                    np.linspace(0.2, 0.9, 24 + 1),
-                    np.linspace(0.9, 1.0, 4 + 1),
-                ]
-            )
-        ),
-        np.linspace(-1.5, 2.0, 40 + 1),
+        np.unique(np.concatenate([
+            np.linspace(0.0, 0.2, 14 + 1),
+            np.linspace(0.2, 0.9, 24 + 1),
+            np.linspace(0.9, 1.0, 4 + 1)
+        ])),
+        np.linspace(-1.5, 2., 40 + 1)
     ),
     "IC79": (
-        np.unique(
-            np.concatenate(
-                [
-                    np.linspace(np.sin(-np.radians(5)), 0.2, 24 + 1),
-                    np.linspace(0.2, 0.9, 24 + 1),
-                    np.linspace(0.9, 1.0, 4 + 1),
-                ]
-            )
-        ),
-        np.linspace(2.0, 7.0, 40 + 1) + 0.2,
+        np.unique(np.concatenate([
+            np.linspace(np.sin(-np.radians(5)), 0.2, 24 + 1),
+            np.linspace(0.2, 0.9, 24 + 1),
+            np.linspace(0.9, 1.0, 4 + 1)
+        ])),
+        np.linspace(2., 7., 40 + 1) + 0.2
     ),
     "IC86": (
-        np.unique(
-            np.concatenate(
-                [
-                    np.linspace(np.sin(-np.radians(5)), 0.2, 24 + 1),
-                    np.linspace(0.2, 0.9, 24 + 1),
-                    np.linspace(0.9, 1.0, 4 + 1),
-                ]
-            )
-        ),
-        np.linspace(2.0, 7.0, 40 + 1),
-    ),
+        np.unique(np.concatenate([
+            np.linspace(np.sin(-np.radians(5)), 0.2, 24 + 1),
+            np.linspace(0.2, 0.9, 24 + 1),
+            np.linspace(0.9, 1.0, 4 + 1)
+        ])),
+        np.linspace(2., 7., 40 + 1)
+    )
 }
 
 
@@ -50,29 +38,50 @@ def get_diffuse_binning(season):
 
 
 class NTSeason(IceCubeSeason):
+
     def get_background_model(self):
         mc = self.load_data(self.mc_path, cut_fields=False)
         mc = rename_fields(mc, {"conv": "weight"})
         return mc
 
     def simulate_background(self):
-        base = self.get_background_model()
+        if isinstance(self.loaded_background_model, type(None)):
+            self.load_background_model()
 
-        n_exp = np.sum(base["weight"])
+        # base = self.get_background_model()
+
+        n_exp = np.sum(self.loaded_background_model["weight"])
+        # n_exp = np.sum(base["weight"])
 
         # Simulates poisson noise around the expectation value n_inj.
         n_bkg = np.random.poisson(n_exp)
 
         # Creates a normalised array of OneWeights
-        p_select = base["weight"] / n_exp
+        p_select = self.loaded_background_model['weight'] / n_exp
+        # p_select = base['weight'] / n_exp
 
         # Creates an array with n_signal entries.
         # Each entry is a random integer between 0 and no. of sources.
         # The probability for each integer is equal to the OneWeight of
         # the corresponding source_path.
-        ind = np.random.choice(len(base["ow"]), size=n_bkg, p=p_select)
+        ind = np.random.choice(len(self.loaded_background_model['ow']), size=n_bkg, p=p_select)
+        # ind = np.random.choice(len(base['ow']), size=n_bkg, p=p_select)
 
         # Selects the sources corresponding to the random integer array
-        sim_bkg = base[ind]
+        sim_bkg = self.loaded_background_model[ind]
+        # sim_bkg = base[ind]
         sim_bkg = sim_bkg[list(self.get_background_dtype().names)]
         return sim_bkg
+
+
+class NTSeasonNewStyle(NTSeason):
+    
+    def get_background_model(self):
+        # in version 3 of the dataset the weights are given as rates in Hz
+        # instead of total events in the associated livetime
+        mc = super(NTSeasonNewStyle, self).get_background_model()
+        livetime = self.get_time_pdf().get_livetime()
+        mc['astro'] *= livetime*86400.
+        mc['weight'] *= livetime*86400.
+        mc['prompt'] *= livetime*86400.
+        return mc
---
+++ flarestack/data/icecube/northern_tracks/nt_v005_p00.py
@@ -0,0 +1,28 @@
+from flarestack.data.icecube.ic_season import IceCubeDataset, icecube_dataset_dir
+from flarestack.data.icecube.northern_tracks import NTSeasonNewStyle, get_diffuse_binning
+
+
+nt_data_dir = icecube_dataset_dir + "northern_tracks/version-005-p00/"
+
+nt_v005_p00 = IceCubeDataset()
+
+sample_name = "northern_tracks_v005_p00"
+
+
+def generate_diffuse_season(name):
+    season = NTSeasonNewStyle(
+        season_name=name,
+        sample_name=sample_name,
+        exp_path=nt_data_dir + f"{name}_exp.npy",
+        mc_path=nt_data_dir + "IC86_pass2_MC.npy",
+        grl_path=nt_data_dir + f"GRL/{name}_exp.npy",
+        sin_dec_bins=get_diffuse_binning(name)[0],
+        log_e_bins=get_diffuse_binning(name)[1]
+    )
+    nt_v005_p00.add_season(season)
+
+
+seasons = [f"IC86_201{i}" for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]]
+
+for season in seasons:
+    generate_diffuse_season(season)
