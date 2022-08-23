45b3b584edb96488bbfa3b55b4ca5fd49a14e225
JannisNe <necker@physik.hu-berlin.de>
2022-08-23 14:36:36 +0200

    correct version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = "2018, Robert Stein"
 author = "Robert Stein"
 
 # The short X.Y version
-version = "2.4.3"
+version = "2.4.5"
 # The full version, including alpha/beta/rc tags
-release = "2.4.3"
+release = "2.4.5"
 
 # -- General configuration ---------------------------------------------------
 
