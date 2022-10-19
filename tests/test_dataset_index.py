68ebbf423ced2966ac7d141d2fd6b4f1284b2c14
Massimiliano Lincetto <m.lincetto@gmail.com>
2022-10-19 16:52:01 +0200

    finally a working test

--- tests/test_dataset_index.py
+++ tests/test_dataset_index.py
@@ -2,13 +2,20 @@ import unittest
 
 from flarestack.data.dataset_index import dataset_index
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 class TestDatasetIndex(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
     def test_index_content(self) -> None:
-        self.assertGreater(len(dataset_index.get_dataset_list()), 0)
+        n = len(dataset_index.get_dataset_list())
+        logger.info(f"There are {n} datasets in the index.")
+        self.assertGreater(n, 0)
 
     def test_index_consistency(self) -> None:
         for key in dataset_index.get_dataset_list():
