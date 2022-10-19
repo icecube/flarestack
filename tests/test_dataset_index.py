f0c51d015f33dc4311d65997adc4577dfa1743af
Massimiliano Lincetto <m.lincetto@gmail.com>
2022-10-19 16:46:39 +0200

    improve testing

--- tests/test_dataset_index.py
+++ tests/test_dataset_index.py
@@ -7,10 +7,13 @@ class TestDatasetIndex(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
-    def test_dataset_index(self) -> None:
+    def test_index_content(self) -> None:
+        self.assertGreater(len(dataset_index.get_dataset_list()), 0)
+
+    def test_index_consistency(self) -> None:
         for key in dataset_index.get_dataset_list():
             dataset = dataset_index.get_dataset(key)
-            self.assertTrue(dataset.name in key)
+            self.assertEqual(key, dataset.name)
 
 
 if __name__ == "__main__":
