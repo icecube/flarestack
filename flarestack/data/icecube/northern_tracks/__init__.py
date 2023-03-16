7602636abb6a7bfb06bb06ac1c4811f0cd59e092
Massimiliano Lincetto <m.lincetto@gmail.com>
2023-03-16 11:39:11 +0100

    more clarity, add numpy rng

--- flarestack/data/icecube/northern_tracks/__init__.py
+++ flarestack/data/icecube/northern_tracks/__init__.py
@@ -58,46 +58,47 @@ class NTSeason(IceCubeSeason):
         """
         mc = self.load_data(self.mc_path, cut_fields=False)
         # According to NT specifications (README):
-        #  "conv" gives the weight for conventional atmospheric neutrinos [1/s]
+        #  "conv" gives the weight for conventional atmospheric neutrinos
         #  flarestack renames it to "weight"
         mc = rename_fields(mc, {"conv": "weight"})
         return mc
 
     def simulate_background(self):
-        if self.loaded_background_model is None: 
-            raise RuntimeError("Monte Carlo background is not loaded. Call `load_background_model` before `simulate_background`.")
+        rng = np.random.default_rng()
 
-        n_exp = np.sum(self.loaded_background_model["weight"]) # 1/s
+        if self.loaded_background_model is None:
+            raise RuntimeError(
+                "Monte Carlo background is not loaded. Call `load_background_model` before `simulate_background`."
+            )
 
-        # Simulates poisson noise around the expectation value n_inj.
-        n_bkg = np.random.poisson(n_exp)
+        n_mc = len(self.loaded_background_model["weight"])
 
-        # Creates a normalised array of OneWeights.
+        # Total number of events in the MC sample, weighted according to background.
+        n_exp = np.sum(self.loaded_background_model["weight"])
+
+        # Creates a normalised array of atmospheric weights.
         p_select = self.loaded_background_model["weight"] / n_exp
-        
-        # Creates an array with n_signal entries.
-        # Each entry is a random integer between 0 and no. of sources.
-        # The probability for each integer is equal to the OneWeight of
-        # the corresponding source_path.
-        ind = np.random.choice(
-            len(self.loaded_background_model["ow"]), size=n_bkg, p=p_select
-        )
-        # ind = np.random.choice(len(base['ow']), size=n_bkg, p=p_select)
 
-        # Selects the sources corresponding to the random integer array
+        # Simulates poisson noise around the expectation value n_exp.
+        n_bkg = rng.poisson(n_exp)
+
+        # Choose n_bkg from n_mc events according to background weight.
+        ind = rng.choice(n_mc, size=n_bkg, p=p_select)
         sim_bkg = self.loaded_background_model[ind]
-        # sim_bkg = base[ind]
-        sim_bkg = sim_bkg[list(self.get_background_dtype().names)]
-        return sim_bkg
+
+        # Reduce the data to the relevant fields for analysis.
+        analysis_keys = list(self.get_background_dtype().names)
+        return sim_bkg[analysis_keys]
 
 
 class NTSeasonNewStyle(NTSeason):
     def get_background_model(self):
-        # in version 3 of the dataset the weights are given as rates in Hz
+        # in version >=3 of the dataset the weights are given as rates in Hz
         # instead of total events in the associated livetime
+        # we deal with this by overwriting the MC set
+        # possibly not the best course of action
         mc = super(NTSeasonNewStyle, self).get_background_model()
         livetime = self.get_time_pdf().get_livetime()
-        mc["astro"] *= livetime * 86400.0
-        mc["weight"] *= livetime * 86400.0
-        mc["prompt"] *= livetime * 86400.0
+        for w_tag in ("astro", "weight", "prompt"):
+            mc[w_tag] *= livetime * 86400.0
         return mc
