c1954a3f371cc1bdaed0db937280b53fbf905e1a
JannisNe <necker@physik.hu-berlin.de>
2025-02-08 14:28:11 +0100

    bump version to 2.4.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = "2018, Robert Stein"
 author = "Robert Stein"
 
 # The short X.Y version
-version = "2.4.9"
+version = "2.4.10"
 # The full version, including alpha/beta/rc tags
-release = "2.4.9"
+release = "2.4.10"
 
 # -- General configuration ---------------------------------------------------
 
